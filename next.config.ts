87f7cbd16dba5b49276f698b2d94234f0f84d27c
Kratos <geniusliuc@gmail.com>
2026-01-06 16:54:48 +0800

    chore: 移除 Next.js 配置中的 basePath 和 NEXT_PUBLIC_BASE_PATH

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/architect",
-  env: {
-    NEXT_PUBLIC_BASE_PATH: "/architect",
-  },
 };
 
 export default nextConfig;
